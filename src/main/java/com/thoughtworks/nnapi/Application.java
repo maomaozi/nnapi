c04ee6195ccade7b96c5bb93c4299c949397b79b
maozi <cat@mmaozi.com>
2019-01-11 14:49:20 +0800

    remove redundant annotation

--- src/main/java/com/thoughtworks/nnapi/Application.java
+++ src/main/java/com/thoughtworks/nnapi/Application.java
@@ -4,12 +4,10 @@ package com.thoughtworks.nnapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @PropertySource(value = {"classpath:properties/amqp-connection.properties"})
 public class Application {
 
