bc8cabaab2085d0562f1f5ba3729ab217e2e070a
maozi <cat@mmaozi.com>
2019-01-14 16:57:29 +0800

    fix typo

--- src/main/java/com/thoughtworks/nnapi/controllers/HWGanController.java
+++ src/main/java/com/thoughtworks/nnapi/controllers/HWGanController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 
 @RestController
 public class HWGanController extends RPCControllerBase {
 
-
-    @RequestMapping(path = "/whgan", method = RequestMethod.POST)
+    @CrossOrigin
+    @RequestMapping(path = "/hwgan", method = RequestMethod.POST)
     public HttpEntity<String> sendMessage(@RequestBody ArrayList<Float> paramList) {
 
         if (paramList.size() != 132) {
